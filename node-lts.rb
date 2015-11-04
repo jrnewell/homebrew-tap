09603641cb23ef98a6139420a7ab2ef463bf1062
James Newell <james.newell@gmail.com>
2015-11-04 09:39:08 -0500

    add node.js LTS stream (4.2.x)

---
+++ node-lts.rb
@@ -0,0 +1,37 @@
+class Node < Formula
+  desc "Platform built on Chrome's JavaScript runtime to build network applications"
+  homepage "https://nodejs.org/"
+  url "https://nodejs.org/dist/v4.2.2/node-v4.2.2.tar.gz"
+  sha256 "00e709353435b436b5bbf5e62272ebb15fe801065422c4c2afe2b93456210cb1"
+
+  option "with-debug", "Build with debugger hooks"
+  option "with-icu4c", "Build with Intl (icu4c) support"
+
+  depends_on "pkg-config" => :build
+  depends_on "icu4c" => :optional
+  depends_on :python => :build if MacOS.version <= :snow_leopard
+
+  def install
+    args = %W[--prefix=#{prefix} --without-npm]
+    args << "--debug" if build.with? "debug"
+    args << "--with-intl=system-icu" if build.with? "icu4c"
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  def caveats
+    <<-EOS.undent
+      Homebrew did NOT install npm.  Please install and configure npm manually.
+    EOS
+  end
+
+  test do
+    path = testpath/"test.js"
+    path.write "console.log('hello');"
+
+    output = `#{bin}/node #{path}`.strip
+    assert_equal "hello", output
+    assert_equal 0, $?.exitstatus
+  end
+end

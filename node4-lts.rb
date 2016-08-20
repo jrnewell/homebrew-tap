8c23d3cdd26e47534f9cc202757aba721fc3ea39
James Newell <james.newell@gmail.com>
2016-08-20 06:30:01 -0400

    update node4-lts.rb formula to v4.5.0

--- node4-lts.rb
+++ node4-lts.rb
@@ -1,15 +1,15 @@
 class Node4Lts < Formula
   desc "JavaScript runtime built on Chrome's V8 engine"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v4.4.7/node-v4.4.7.tar.xz"
-  sha256 "1ef900b9cb3ffb617c433a3247a9d67ff36c9455cbc9c34175bee24bdbfdf731"
+  url "https://nodejs.org/dist/v4.5.0/node-v4.5.0.tar.xz"
+  sha256 "97b99d378c56802444208409568e2e66c46332897f06aead74d1ffbe733bd488"
   head "https://github.com/nodejs/node.git", :branch => "v4.x-staging"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-versions"
-    sha256 "e55b810c3f0be1c3f8f1a014462b23d3088301bb54832ca1bb1e1b3c634c583f" => :el_capitan
-    sha256 "d3600f7c983a5f4fae53b535db390366604428f9a7022ad2862581d324ceaedb" => :yosemite
-    sha256 "b6f03de06a244668dec0001598771f97e266bcfb1cf1607068f43a4715935fa3" => :mavericks
+    sha256 "5c0373c7b369389a9c8fb89037160b74723b23e9d614d4e96f93b02b0ac6bf82" => :el_capitan
+    sha256 "391f6faa98e1739cfee4ac418b4437f5f770c1c383969e8a00bd230e450aa06b" => :yosemite
+    sha256 "c8fa7cec62107cb73c7ecc19987cd76b3570a383588ab4ecb060f13b8f05a470" => :mavericks
   end
 
   option "with-debug", "Build with debugger hooks"

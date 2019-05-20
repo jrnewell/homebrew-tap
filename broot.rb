df75b1bfa481d2ef52f70ce7d01decaa6969f2b6
James Newell <james.newell@gmail.com>
2019-05-20 14:52:32 -0400

    update broot version v0.7.5

--- broot.rb
+++ broot.rb
@@ -1,8 +1,8 @@
 class Broot < Formula
   desc "An interactive tree view, a fuzzy search, a balanced BFS descent and customizable commands"
   homepage "https://github.com/Canop/broot/"
-  url "https://github.com/Canop/broot/archive/v0.7.1.tar.gz"
-  sha256 "a16e471527fa7283e228946574ec55a2c73f951b7035801f671895815f348f68"
+  url "https://github.com/Canop/broot/archive/v0.7.5.tar.gz"
+  sha256 "65635be08f3959747f830ada0891cbb800353e8268e2260ea783ad519113a57e"
   head "https://github.com/Canop/broot.git"
 
   depends_on "rust" => :build
